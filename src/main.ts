a0182bee0ab8e3af32ec02c53f43baccb5fa5d46
SULAPIS <sulapis@outlook.com>
2024-02-22 09:16:01 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,100 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { context, getOctokit } from '@actions/github'
+import type { GraphQlQueryResponseData } from '@octokit/graphql'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const accessToken = core.getInput('github-token')
+    const milestone = parseInt(core.getInput('milestone'))
+    const issuesCount = parseInt(core.getInput('issues-count'))
+    const owner = context.payload.repository?.owner.login!
+    const name = context.payload.repository?.name!
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const client = getOctokit(accessToken)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const { check } = await client.graphql<GraphQlQueryResponseData>({
+      query: `query checkMilestoneExists($owner: String!, $name: String!, $milestone: Int) {
+        repository(owner: $owner, name: $name) {
+          milestone(number: $milestone) {
+            title
+          }
+        }
+      }`,
+      owner: owner,
+      name: name,
+      milestone: milestone + 1
+    })
+    const nextMilestoneExists = check.repository.milestone != null
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const { repository } = await client.graphql<GraphQlQueryResponseData>({
+      query: `query issues($owner: String!, $name: String!, $milestone: Int, $first: Int) {
+        repository(owner: $owner, name: $name) {
+          milestone(number: $milestone) {
+            title
+            issues(first: $first, states: OPEN) {
+              nodes {
+                labels(first: 20) {
+                    nodes {
+                        name
+                    }
+                }
+                number
+              }
+            }
+          }
+        }
+      }`,
+      owner: owner,
+      name: name,
+      milestone: milestone,
+      first: issuesCount
+    })
+
+    type Issue = {
+      number: number
+      labels: {
+        nodes: {
+          name: string
+        }[]
+      }
+    }
+    const issues: Issue[] = repository.milestone.issues.nodes
+    const milestoneTitle: String = repository.milestone.title
+    const missedLabel = 'missed: v' + milestoneTitle
+
+    if (issues.length != 0) {
+      await client.request('POST /repos/{owner}/{repo}/labels', {
+        owner: owner,
+        repo: name,
+        name: missedLabel,
+        color: '497E76',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
+    }
+
+    for (const issue of issues) {
+      const issueNumber = issue.number
+      const issueHasMissedLabel = issue.labels.nodes.some(label =>
+        label.name.includes('missed: v')
+      )
+      if (!issueHasMissedLabel) {
+        await client.rest.issues.addLabels({
+          owner,
+          repo: name,
+          issue_number: issueNumber,
+          labels: [missedLabel]
+        })
+      }
+      const updateMilestone = nextMilestoneExists ? milestone + 1 : null
+      await client.rest.issues.update({
+        owner,
+        repo: name,
+        issue_number: issueNumber,
+        milestone: updateMilestone
+      })
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
